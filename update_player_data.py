16ece2e7eb3d5c8a0d008eb8d27cff3a743a90f5
masonkimbell <mkimbell7@pa.metrocast.net>
2023-10-17 20:47:07 -0400

    fixed error in update script

--- update_player_data.py
+++ update_player_data.py
@@ -3,6 +3,9 @@ from data_loaders import load_players
 
 players = load_players()
 
+with open('assets/map.json', 'r') as f:
+    map = json.load(f)
+
 for player in players:
     for pkmn in players[player]['dex']:
         form_list = []
@@ -11,11 +14,20 @@ for player in players:
             found_in = ['hoenn']
             dexno = int(pkmn)
             if dexno >= 1 and dexno <= 151:
-                form_list = ['kanto']
+                try:
+                    map[pkmn]
+                except KeyError:
+                    form_list = ['kanto']
             elif dexno >= 152 and dexno <= 251:
-                form_list = ['johto']
+                try:
+                    map[pkmn]
+                except KeyError:
+                    form_list = ['johto']
             elif dexno >= 252 and dexno <= 386:
-                form_list = ['hoenn']
+                try:
+                    map[pkmn]
+                except KeyError:
+                    form_list = ['hoenn']
         players[player]['dex'][pkmn]['found_in'] = found_in      
         players[player]['dex'][pkmn]['forms'] = form_list
 
